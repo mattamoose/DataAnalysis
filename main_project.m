2bf07d80c527996a89a05c2031f2e6f077f42878
afton <afton@DESKTOP-B5GAHR1>
2024-12-06 17:30:48 -0700

    updated

--- main_project.m
+++ main_project.m
@@ -1,9 +1,9 @@
 clc; close all; clear  
 
-data = readtable('146400 2024-04-01 2024-10-31 0-Minute Average.csv');
+data = readtable('146400-09-25-11-25.csv');
 
 chA = table2array(data(:,2)); chB = table2array(data(:,3));
-
+s = '';
 %%
 %Data Span 9/25 00:01 - 11/24 23:59  
 dt =  2; % mins
@@ -29,16 +29,20 @@ plot(t,y)
 title('Mean of Channels')
 xlabel('time (min)')
 ylabel('PM2.5 Mass Concentration (ug/m^3)')
+A = [ones(size(t))'  t'];
+x = A\y;
+fprintf([' \n The linear trend of the data is as follows: \n %10.1s ' ...
+    'Intercept = %6.4f \n %10.1s Slope = %6.6f \n\n'],s,x(1),s,x(2))
+ym = A*x;
+hold on
+plot(t,ym)
 
-y = detrend(chB);
-
-mean = mean(y);
-disp(mean)
-
+y = y - ym;
 figure(3)
 plot(t,y)
 title('Detrended Data Set')
-
+dty = mean(y);
+fprintf('\n The mean of the detrended data set = %8.8f',dty)
 figure(4)
 
 [f,Pyy] = psd(n,df,y);
@@ -48,33 +52,77 @@ xscale('log')
 
 dom_freq = f(Pyy == max(Pyy));
 freq_days = dom_freq*60*24;
-fprintf('The dominant frequency of PM2.5 is %4.5f cycles/day \n\n',freq_days)
+fprintf(' \n \n The dominant frequency of PM2.5 is %4.5f cycles/day \n',freq_days)
 
 fprintf('Or a period of %4.2f days \n',1/freq_days)
+%% Band-Pass Filter
 
-dom_freq2 = 9.513e-5;
-freq_days = dom_freq2*60*24;
-fprintf('The third dominant frequency of PM2.5 is %4.5f cycles/day \n\n',freq_days)
-
-fprintf('Or a period of %4.2f days \n',1/freq_days)
-%% High-Pass Filter
-
-cutoff = dom_freq+10*df/n;
+band = [dom_freq-1e-5*df/n,dom_freq+1e-5*df/n];
  
-f_l = fir1(50,cutoff,"high");
+f_bd = fir1(20,band,'bandpass');
 
-y_l = filter(f_l,1,y);
+y_l = filter(f_bd,1,y);
 figure(6)
 plot(t,y_l)
-hold on
-title('Low Pass Filtered Series')
+title('Band Pass Filtered Series')
 
 [f,Py_l] = psd(n,df,y_l);
-figure(7)
+figure(4)
+hold on
 plot(f,Py_l)
-title('PSD of Low-Pass Filter')
+title('PSD of Band-Pass Filter')
 xscale('log')
+legend('Psd unfiltered','psd filtered')
 
+ex = mean(y_l);
+fprintf(' \n The mean contribution is %8.8f',ex)
+
+%% Isolate Commuter traffic band
+
+bandc = 1./[13,9]/60; % cycles/min
+f_bc = fir1(20,bandc,'bandpass');
+
+y_c = filter(f_bc,1,y);
+
+Pyy(f < bandc(1)) = 0; Pyy(f > bandc(2));
+yc = invpsd(n,Pyy,'symmetric');
+I = (yc>30);
+yc(I) = [];
+tc = t; tc(I) = [];
+
+figure(7)
+plot(tc,yc)
+hold on
+plot(t,y_c)
+legend('Crude','FIR')
+title('Commuter Band Pass')
+
+cc = mean(yc);
+
+fprintf('\n The mean contribution from commuter traffic is %8.8f',cc)
+
+figure(8)
+
+plot(f,Pyy)
+hold on
+[f,pyyc] = psd(n,df,y_c);
+plot(f,pyyc)
+xscale('log')
+legend('Psd unfiltered','psd filtered')
+
+figure(9)
+
+spectrogram(y_c)
+
+figure(10)
+cwt(y_c)
+figure(11)
+cwt(y)
+
+figure(12)
+[ps,fp] = periodogram(y,[],[],df);
+dom = fp(ps == max(ps))*60*24;
+fprintf('\nUsing periodogram, the dominant frequency has a period of %4.4f',1/dom)
 
 function [f,Pxx] = psd(n,df,y)
     ffte = fft(y);
@@ -82,4 +130,10 @@ function [f,Pxx] = psd(n,df,y)
     Pyy = Py(1:floor(n/2)+1);
     Pxx = [Pyy(1);2*Pyy(2:end)];
     f = 0:df/n:df/2; % cycles/time
+end
+
+function [yr] = invpsd(n,Poa,invtype)
+    Pxx = [sqrt(Poa(1)*n); sqrt(Poa(2:end)/2*n); sqrt(fliplr(conj(Poa(2:end)/2*n)))];
+    yr = ifft(Pxx,n,invtype);
+
 end
